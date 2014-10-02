b6a7fd6a1076bf9af3f953dd0223c08767f33604
Adam Maxwell <catalyst256@gmail.com>
2014-10-02 07:14:51 +0100

    Tweaks to haveibeenpwned code

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -7,33 +7,41 @@ import json
 email = sys.argv[1]
 
 def check_email(email):
-  url = 'https://haveibeenpwned.com/api/v2/breachedaccount/'
-  req = url + str(email)
-  print req
-  r = requests.get(req, verify=False)
-  if r.status_code == 404:
-    print 'Account: ' + email + ' has not been pwned'
-  if r.status_code == 200:
-    j = r.json()
-    x = json.dumps(j, indent=2, separators=(',', ': '), ensure_ascii=False, encoding="utf-8")
-    print x
-  else:
-    print r.status_code
+  try:
+    url = 'https://haveibeenpwned.com/api/v2/breachedaccount/'
+    req = url + str(email)
+    r = requests.get(req, verify=False)
+    if r.status_code == 404:
+      x = 'Account: ' + email + ' has not been pwned'
+      return x
+    if r.status_code == 200:
+      j = r.json()
+      x = json.dumps(j, indent=2, separators=(',', ': '), ensure_ascii=False, encoding="utf-8")
+      return x
+    else:
+      pass
+  except Exception as e:
+    return str(e)
 
 def check_pastebin(email):
-  url = 'https://haveibeenpwned.com/api/v2/pasteaccount/'
-  req = url + str(email)
-  print req
-  r = requests.get(req, verify=False)
-  if r.status_code == 404:
-    print 'Account: ' + email + ' has not been pasted'
-  if r.status_code == 200:
-    j = r.json()
-    x = json.dumps(j, indent=2, separators=(',', ': '), ensure_ascii=False, encoding="utf-8")
-    print x
-  else:
-    print r.status_code
+  try:
+    url = 'https://haveibeenpwned.com/api/v2/pasteaccount/'
+    req = url + str(email)
+    r = requests.get(req, verify=False)
+    if r.status_code == 404:
+      x = 'Account: ' + email + ' has not been pasted'
+      return x
+    if r.status_code == 200:
+      j = r.json()
+      x = json.dumps(j, indent=2, separators=(',', ': '), ensure_ascii=False, encoding="utf-8")
+      return x
+    else:
+      pass
+  except Exception as e:
+    return str(e)
 
 
-check_email(email)
-check_pastebin(email)
+t = check_email(email)
+print t
+q = check_pastebin(email)
+print q
